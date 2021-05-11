b8a56f0f3fca4121c4c597882fd139944f9c1144
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-11 16:16:24 +0900

    add submission: 2021/05/11 16:16:22

---
+++ submissions/abc136/a.py
@@ -0,0 +1,8 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+a, b, c = map(int, read().split())
+print(max(0, c - (a - b)))
