71a460a576b39324b388a1b92fbbe4aa53444d9e
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-11 15:45:43 +0900

    add submission: 2021/05/11 15:45:40

---
+++ submissions/abc012/c.py
@@ -0,0 +1,10 @@
+# sys.stdin.readline()
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+check = 2025-n
+for i in range(1,10):
+    for j in range(1,10):
+        if check == i*j:
+            print(str(i)+' x '+str(j))
