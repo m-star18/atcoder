76f6cd0a6503127c3db1c441236339a408fddbb0
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-11 15:16:01 +0900

    add submission: 2021/05/11 15:15:59

---
+++ submissions/abc115/a.py
@@ -0,0 +1,12 @@
+d = int(input())
+
+if d == 25:
+    ans = 'Christmas'
+elif d == 24:
+    ans = 'Christmas Eve'
+elif d == 23:
+    ans = 'Christmas Eve Eve'
+elif d == 22:
+    ans = 'Christmas Eve Eve Eve'
+    
+print(ans)
