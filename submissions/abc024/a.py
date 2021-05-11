1255fe8f3f03aa91dad3674277a0e372ff395ac2
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-11 16:51:12 +0900

    add submission: 2021/05/11 16:51:09

---
+++ submissions/abc024/a.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+a, b, c, k = map(int, input().split())
+s, t = map(int, input().split())
+ans = a * s + b * t
+
+if s + t >= k:
+    ans -= c * (s + t)
+
+print(ans)
