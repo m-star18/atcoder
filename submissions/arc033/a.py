d1d32e92ed4fe850e18a154da1351e1fabe9b7fc
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 12:03:58 +0900

    add submission: 2021/05/12 12:03:55

---
+++ submissions/arc033/a.py
@@ -0,0 +1,8 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+n = int(readline())
+print(n * (n + 1) // 2)
