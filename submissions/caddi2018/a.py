713d3594968cfe4f2f7186372d524a50dd35f274
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 11:40:58 +0900

    add submission: 2021/05/12 11:40:55

---
+++ submissions/caddi2018/a.py
@@ -0,0 +1,28 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+from collections import Counter
+
+
+def prime_decomposition(n):
+    i = 2
+    table = []
+    while i * i <= n:
+        while n % i == 0:
+            n //= i
+            table.append(i)
+        i += 1
+    if n > 1:
+        table.append(n)
+    return table
+
+
+n, p = map(int, read().split())
+ans = 1
+for k, v in Counter(prime_decomposition(p)).items():
+    v //= n
+    ans *= k ** v
+print(ans)
