a9ccbc740a0907a1cdbbc274131efcfe8f8f4ec6
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 11:54:51 +0900

    add submission: 2021/05/12 11:54:48

---
+++ submissions/nikkei2019-2-qual/a.py
@@ -0,0 +1,10 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+from math import floor
+
+n = int(readline())
+print(floor((n - 1) / 2))
