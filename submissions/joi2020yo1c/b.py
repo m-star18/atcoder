073c5d49dc6443783c83f4f90016c94ccbf1bdac
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 13:34:42 +0900

    add submission: 2021/05/12 13:34:39

---
+++ submissions/joi2020yo1c/b.py
@@ -0,0 +1,9 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+n = int(readline())
+s = readline().rstrip().decode()
+print(s.replace('joi', 'JOI'))
