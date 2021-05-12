46e7464bf43600653544855cdc04afcf3a4d45c7
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 13:38:08 +0900

    add submission: 2021/05/12 13:38:06

---
+++ submissions/soundhound2018-summer-qual/b.py
@@ -0,0 +1,9 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+s = readline().rstrip().decode()
+w = int(readline())
+print(s[::w])
