09630b65665a5f7c75c48a0f905e64ee4a0cc64f
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 13:37:02 +0900

    add submission: 2021/05/12 13:36:58

---
+++ submissions/chokudai_S001/j.py
@@ -0,0 +1,56 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+
+class Bit:
+    def __init__(self, n):
+        self.size = n
+        self.tree = [0]*(n+1)
+
+    def __iter__(self):
+        psum = 0
+        for i in range(self.size):
+            csum = self.sum(i+1)
+            yield csum - psum
+            psum = csum
+        raise StopIteration()
+
+    def __str__(self):  # O(nlogn)
+        return str(list(self))
+
+    def sum(self, i):
+        # [0, i) の要素の総和を返す
+        if not (0 <= i <= self.size): raise ValueError("error!")
+        s = 0
+        while i>0:
+            s += self.tree[i]
+            i -= i & -i
+        return s
+
+    def add(self, i, x):
+        if not (0 <= i < self.size): raise ValueError("error!")
+        i += 1
+        while i <= self.size:
+            self.tree[i] += x
+            i += i & -i
+
+    def __getitem__(self, key):
+        if not (0 <= key < self.size): raise IndexError("error!")
+        return self.sum(key+1) - self.sum(key)
+
+    def __setitem__(self, key, value):
+        # 足し算と引き算にはaddを使うべき
+        if not (0 <= key < self.size): raise IndexError("error!")
+        self.add(key, value - self[key])
+
+
+n, *a = map(int, read().split())
+bit = Bit(n + 1)
+ans = 0
+for i, aa in enumerate(a):
+    ans += i - bit.sum(aa + 1)
+    bit.add(aa, 1)
+print(ans)
