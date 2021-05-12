05555c63321cfb3eb1726ab14660d15360ba3a5a
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 11:52:11 +0900

    add submission: 2021/05/12 11:52:08

---
+++ submissions/abc145/a.py
@@ -0,0 +1,8 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+r = int(readline())
+print(r ** 2)
