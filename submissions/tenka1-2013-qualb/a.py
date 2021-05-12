c251999caaf7cd51a40f4a58c85e4532ab3ab73f
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 13:48:45 +0900

    add submission: 2021/05/12 13:48:42

---
+++ submissions/tenka1-2013-qualb/a.py
@@ -0,0 +1,7 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+print(sorted([readline().rstrip().decode() for _ in range(15)])[6])
