e632f03412a8240dc57fec5ba2abd97242dd799e
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 12:01:47 +0900

    add submission: 2021/05/12 12:01:44

---
+++ submissions/abc154/c.py
@@ -0,0 +1,12 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+n = int(readline())
+a = list(map(int, readline().split()))
+if len(set(a)) == n:
+    print('YES')
+else:
+    print('NO')
