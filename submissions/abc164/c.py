3cd4946d90e673534568a7ee8e82d0aae2ce80ff
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 13:40:42 +0900

    add submission: 2021/05/12 13:40:36

---
+++ submissions/abc164/c.py
@@ -0,0 +1,9 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+n = int(readline())
+s = [readline().rstrip().decode() for _ in range(n)]
+print(len(set(s)))
