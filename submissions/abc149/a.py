3dbfaee1a2b8e3bd322bdf9e5636d87df533c8a2
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 11:57:14 +0900

    add submission: 2021/05/12 11:57:11

---
+++ submissions/abc149/a.py
@@ -0,0 +1,8 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+s, t = input().split()
+print(t + s)
