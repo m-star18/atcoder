3fe3392dd338607b436b4e58bffa53332e881a91
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 13:29:15 +0900

    add submission: 2021/05/12 13:29:12

---
+++ submissions/joi2010yo/a.py
@@ -0,0 +1,9 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+n = int(readline())
+a = [int(readline()) for i in range(9)]
+print(n - sum(a))
