90558c715ec2509711f08a6a4f2ad30ca8a27c00
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 11:53:16 +0900

    add submission: 2021/05/12 11:53:12

---
+++ submissions/abc146/f.py
@@ -0,0 +1,25 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+from collections import deque
+
+n, m = map(int, readline().split())
+s = readline().rstrip().decode()[::-1]
+index = 0
+ans = deque([])
+for i in range(n):
+    for j in range(m, 0, -1):
+        if index + j >= n:
+            ans.appendleft(n - index)
+            print(*ans)
+            exit()
+        if s[index + j] == '0':
+            ans.appendleft(j)
+            index += j
+            break
+    else:
+        print(-1)
+        exit()
