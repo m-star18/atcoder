19a796b2cc04521c40a135d6a8a7d9723c82e151
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 11:41:43 +0900

    add submission: 2021/05/12 11:41:40

---
+++ submissions/abc141/b.py
@@ -0,0 +1,11 @@
+s = input()
+for i in range(len(s)):
+    if i % 2 == 0:
+        if s[i] == 'L':
+            print('No')
+            exit()
+    else:
+        if s[i] == 'R':
+            print('No')
+            exit()
+print('Yes')
