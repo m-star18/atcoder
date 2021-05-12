62ed2593d5023890fb31770d9a11c671f339f5a8
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 12:11:21 +0900

    add submission: 2021/05/12 12:11:18

---
+++ submissions/abc163/a.py
@@ -0,0 +1,9 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+from math import pi
+
+print(2 * int(read()) * pi)
