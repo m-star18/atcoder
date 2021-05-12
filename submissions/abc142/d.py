28fb841f9c1aa3055e427eb9bdfa0a3f219b015c
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 11:42:34 +0900

    add submission: 2021/05/12 11:42:31

---
+++ submissions/abc142/d.py
@@ -0,0 +1,35 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+
+def prime_decomposition(n):
+    i = 2
+    table = []
+    while i * i <= n:
+        while n % i == 0:
+            n //= i
+            table.append(i)
+        i += 1
+    if n > 1:
+        table.append(n)
+    return table
+
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+a, b = map(int, read().split())
+cnt = 1
+for n in set(prime_decomposition(b)) & set(prime_decomposition(a)):
+    if is_prime(n):
+        cnt += 1
+print(cnt)
