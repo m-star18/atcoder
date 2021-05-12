81af7f1f4fddff065d0bf5d8f54fd2132b5a465d
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 11:36:46 +0900

    add submission: 2021/05/12 11:36:43

---
+++ submissions/caddi2018b/a.py
@@ -0,0 +1,8 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+s = read().rstrip().decode()
+print(s.count('2'))
