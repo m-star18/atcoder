99179a0536a309a7e1c72fcd0a938224f3ab7bb8
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 13:36:12 +0900

    add submission: 2021/05/12 13:36:09

---
+++ submissions/chokudai_S001/c.py
@@ -0,0 +1,9 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+n = int(readline())
+a = readline().rstrip().decode()
+print(a.replace(' ', ','))
