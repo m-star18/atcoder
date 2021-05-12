5764b289520b7c4d12a923092d99cbf86edbdda6
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 11:44:35 +0900

    add submission: 2021/05/12 11:44:31

---
+++ submissions/abc143/b.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+d = list(map(int, input().split()))
+ans = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        ans += d[i] * d[j]
+print(ans)
