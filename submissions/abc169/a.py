faf0e7ff4f8e31d86577899d0d6a59e4fb14407b
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 13:51:20 +0900

    add submission: 2021/05/12 13:51:17

---
+++ submissions/abc169/a.py
@@ -0,0 +1,8 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+a, b = map(int, readline().split())
+print(a * b)
