727fb33fd80157f0076d50badc501a7ec586b3c5
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 11:58:53 +0900

    add submission: 2021/05/12 11:58:50

---
+++ submissions/abc150/b.py
@@ -0,0 +1,9 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+n = int(readline())
+s = input()
+print(s.count('ABC'))
