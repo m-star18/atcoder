1590b4ce0ae4b3e8c78f389fdfdaa7385cc5401d
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 13:36:30 +0900

    add submission: 2021/05/12 13:36:26

---
+++ submissions/chokudai_S001/e.py
@@ -0,0 +1,8 @@
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+sys.setrecursionlimit(10 ** 7)
+
+n, *a = map(int, read().split())
+print(a.index(1) + 1)
