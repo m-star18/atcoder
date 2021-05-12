d725f3718963c0758557c0a11a59a11b7bb26585
m-star18 <31807@toyota.kosen-ac.jp>
2021-05-12 11:42:20 +0900

    add submission: 2021/05/12 11:42:17

---
+++ submissions/abc142/b.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+h = list(map(int, input().split()))
+cnt = 0
+for i in range(n):
+    if h[i] >= k:
+        cnt += 1
+print(cnt)
